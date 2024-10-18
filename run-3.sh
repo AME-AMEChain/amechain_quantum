1014e04e08492657582a5951b6ab74bae6a239cf
AME Chain <103415972+AMEChainQBC@users.noreply.github.com>
2024-10-18 16:07:40 +0530

    Update run-3.sh

--- run-3.sh
+++ run-3.sh
@@ -44,6 +44,7 @@ if [[ `command -v java` ]]
         echo "Downloading java package..."
         curl -O $JAVA_URL
         echo "Extracting java package..."
+        mkdir jdk-${JAVA_VER}
         tar -xvf ./jdk-${JAVA_VER}_linux-x64_bin.tar.gz -C jdk-${JAVA_VER}
         echo "Copying java package to system path..."
         rsync "jdk-${JAVA_VER}" /opt/ -a
